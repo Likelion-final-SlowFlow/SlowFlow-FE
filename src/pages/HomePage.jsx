d17c7621b6af89c577ce574caa862a1945852b40
seeun <tpdms3139@naver.com>
2025-12-23 02:05:16 +0900

    Design/#9/홈 페이지 구현

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React from 'react'
+import Score from '@/components/home/Score'
+import SelectCategory from '@/components/home/SelectCategory'
 
 const HomePage = () => {
-  return <div>HomePage</div>;
-};
+  return (
+    <div className='screen-center'>
+      <div className='mb-4 ml-[15%] self-start'>
+        <p className='text-xl font-semibold text-black'>오늘의 목표 도달까지</p>
+        <p className='text-xl font-semibold text-black'>200점 남았어요!</p>
+      </div>
+      <Score />
+      <SelectCategory />
+    </div>
+  )
+}
 
-export default HomePage;
+export default HomePage
