98fb140fc725b070bf52785936a12a1edd4e9681
kyooonnnggg <gyoeunjung23@sookmyung.ac.kr>
2025-12-24 19:20:21 +0900

    API : ID 중복 확인

---
+++ src/api/shared/checkUsername.js
@@ -0,0 +1,13 @@
+import client from './client'
+import { handleApiError } from './handleApiError.js'
+
+export async function checkUsername(username) {
+  try {
+    const res = await client.get('/auth/check-username', {
+      params: { username },
+    })
+    return res.data.data === true // 사용 가능하면 true, 아니면 false 반환
+  } catch (error) {
+    throw handleApiError(error)
+  }
+}
